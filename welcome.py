0f8bda1fcc7b389de52e76be894330a4256cfa62
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-08 00:51:36 +0100

    Add title banner with pyfiglet

--- welcome.py
+++ welcome.py
@@ -1,8 +1,15 @@
-'''Welcome message and introduction'''
+'''Handle import and banner'''
+import pyfiglet 
 def welcome_intro():
 
-    print('Welcome to your Mortgage Advisor!\n'.center(50))
-    print('=========\n'.center(50))
+    '''Welcome message and introduction'''
+
+    title = 'Mortgage Advisor'
+    banner = pyfiglet.figlet_format(title, font='banner3-D', width=90, justify='center')
+    print(banner)
+
+    print('Welcome to your Mortgage Advisor!'.center(90))
+    print('============================\n'.center(90))
     print('Are you looking to buy a house in Ireland?')
     print('And you are a First Time Buyer (FTB)')
     print('or a Second & Subsequent Buyer (SSB)?')
