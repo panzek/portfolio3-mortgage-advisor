08b9267a6568f3bf872ec8685cdfa29962795d82
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-08 11:32:42 +0100

    Update username function to get data from user

--- user.py
+++ user.py
@@ -3,5 +3,9 @@
 def user_name():
     """username function to get data from user"""
 
-    print('This is my username')
-    
+    print('Username must be between 4 and 10 characters')
+    print('Characters A-Z, a-z, 0-9 and spaces are permitted')
+    print('Leading and trailing whitespaces will be removed\n')
+
+    new_user = input('Please enter your username: ')
+    print(f'Thank you and welcome, {new_user}')
