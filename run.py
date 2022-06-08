042e641f4f64aef2c9531e9a5f2019fe060c6ba2
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-08 11:12:07 +0100

    Create user name function and import to run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ obtain credentials using service account private key file
 import gspread
 from google.oauth2.service_account import Credentials
 import welcome
+import user
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,9 +18,5 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENTS = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENTS.open('mortgage_advisor')
 
-first_time_buyer = SHEET.worksheet('first_time_buyer')
-
-data = first_time_buyer.get_all_values()
-
 welcome.welcome_intro()
-print(data)
+user.user_name()
---
+++ user.py
@@ -0,0 +1,7 @@
+"""create user name"""
+
+def user_name():
+    """username function to get data from user"""
+
+    print('This is my username')
+    
