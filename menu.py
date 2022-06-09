8ab1e744c6b627e0935ef7cfea2d536f5122f734
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-09 21:46:32 +0100

    Create check menu list method to handle user inputs

--- menu.py
+++ menu.py
@@ -5,11 +5,21 @@ def menu_list():
 
     while True:
         print('\nPlease choose one of the following options:')
-        print('\n1. To run an overview of the application')
+        print('\n1. To run an overview of this application')
         print('2. To run through the instructions')
-        print('3. To run new enquiry')
-        print('4. To delete all existing enquiry results')
+        print('3. To run new mortgage enquiry')
+        print('4. To delete existing mortgage enquiry results')
         print('9. To exit the application\n')
 
-        int(input('Please enter your option: 1,2,3,4, or 9: \n'))
-        
+        list_option = int(input('Please enter your option: 1,2,3,4, or 9: \n'))
+        check_menu_list(list_option)
+
+def check_menu_list(list_option):
+    """ handle user inputs and errors"""
+
+    if list_option == 3:
+        print('\nPlease select your mortgage type: 1 or 2')
+        print('1. First Time Buyer (FTB)')
+        print('2. Second & Subsequent Buyer (SSB)\n')
+
+        int(input('Please enter your option: \n'))
