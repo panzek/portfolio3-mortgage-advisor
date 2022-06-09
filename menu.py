50e20a6fc7330bf8d08d77a9f18dedf7b55aa8fb
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-09 22:44:46 +0100

    Add code to check menu list to display options

--- menu.py
+++ menu.py
@@ -9,13 +9,51 @@ def menu_list():
         print('2. To run through the instructions')
         print('3. To run new mortgage enquiry')
         print('4. To delete existing mortgage enquiry results')
-        print('9. To exit the application\n')
+        print('5. To exit the application\n')
 
-        list_option = int(input('Please enter your option: 1,2,3,4, or 9: \n'))
+        list_option = int(input('Please enter your option: 1,2,3,4, or 5: \n'))
         check_menu_list(list_option)
 
 def check_menu_list(list_option):
     """ handle user inputs and errors"""
+
+    if list_option == 1:
+        print('\nOur Application Overview')
+        print('==================')
+        print('\nOur application will show how much your')
+        print('mortgage repayment will cost you')
+        print('when you borrow to get a mortgage,')
+        print('depending on the amount you borrow,')
+        print('interest rate, and the mortgage term.\n')
+
+        input('Press any key to continue... \n')
+
+        print('It gives the user, either as a First Time')
+        print('Buyer (FTB) or a Second and Subsequent')
+        print('Buyer (SSB) an overview of the new mortgage')
+        print('lending in Ireland as published by the')
+        print('Central Bank of Ireland in 2022 and estimates.')
+        print('the user monthly repayments.\n')
+
+        input('Press any key to return to main menu... \n')
+
+    if list_option == 2:
+        print('\nInstructions')
+        print('==================\n')
+        print('How to use our mortgage calculator?\n')
+        print('To start, choose your mortgage type:')
+        print('First Time Buyer (FTB) or')
+        print('Second & Subsequent Buyer (SSB)\n')
+
+        input('Press any key to continue... \n')
+
+        print('How much will the repayments be?\n')
+        print('Estimate your mortgage repayments by ')
+        print('entering your property price, how ')
+        print('much you would like to borrow ')
+        print('and over what period (loan term).\n')
+
+        input('Press any key to return to main menu... \n')
 
     if list_option == 3:
         print('\nPlease select your mortgage type: 1 or 2')
@@ -23,3 +61,13 @@ def check_menu_list(list_option):
         print('2. Second & Subsequent Buyer (SSB)\n')
 
         int(input('Please enter your option: \n'))
+
+    if list_option == 4:
+        print('\nDelete existing mortgage enquiry results')
+
+        input('\nPress any key to delete \n')
+
+    if list_option == 5:
+        print('\nExit the application')
+
+        input('\nPress any key to Exit \n')
