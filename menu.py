2e8227ea2e0fc5bb900928b3c34de7ed0cd21140
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-09 20:48:28 +0100

    Update menu list with list options

--- menu.py
+++ menu.py
@@ -3,4 +3,13 @@
 def menu_list():
     """ List of options """
 
-    print('\nPlease choose one of the following: 1,2,3,4, or 9')
+    while True:
+        print('\nPlease choose one of the following options:')
+        print('\n1. To run an overview of the application')
+        print('2. To run through the instructions')
+        print('3. To run new enquiry')
+        print('4. To delete all existing enquiry results')
+        print('9. To exit the application\n')
+
+        int(input('Please enter your option: 1,2,3,4, or 9: \n'))
+        
