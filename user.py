68ecdf2d98ca2d7e8b8e499d80dd9833e09d93ad
Peter Anny-Nzekwue <panzek@yahoo.co.uk>
2022-06-11 13:03:49 +0100

    Add code to check new and returning user

--- user.py
+++ user.py
@@ -13,6 +13,8 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENTS = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENTS.open('mortgage_advisor')
 
+first_time_buyer_sheet = SHEET.worksheet('first_time_buyer')
+
 def user_name():
     """username function to get data from user"""
 
@@ -21,15 +23,16 @@ def user_name():
         print('Characters A-Z, a-z, 0-9 and spaces are permitted')
         print('Leading and trailing whitespaces will be removed\n')
 
-        new_user = input('Please enter your username: \n').lower()
+        username = input('Please enter your username: ').lower()
 
-        if validate_username(new_user):
-            print(f'{chr(10)}Thank you and welcome, {new_user}')
+        if validate_username(username):
+            check_username(username)
+
             break
 
-    return new_user.strip()
+    return username.strip()
 
-def validate_username(new_user):
+def validate_username(username):
     """
     This will check if the username input 
     data is valid or not, and will raise exception 
@@ -37,13 +40,13 @@ def validate_username(new_user):
     """
 
     try:
-        if not new_user:
+        if not username:
             raise ValueError('You must enter a username')
-        if len(new_user) < 4:
+        if len(username) < 4:
             raise ValueError(
                 'Username must have at least 4 characters'
             )
-        if len(new_user) > 10:
+        if len(username) > 10:
             raise ValueError(
                 'Username must not exceed 10 characters'
             )
@@ -51,11 +54,22 @@ def validate_username(new_user):
     except ValueError as error_msg:
         print(f'Invalid Data: {error_msg}, please try again{chr(10)}')
         return False
-
+    
     return True
 
-def check_username():
+def check_username(username):
     """
     Check if username exist and allow access to existing
     data. If there is no such username create a username
     """
+
+    print(f'{chr(10)}Checking if {username} exist...')
+    existing_user = first_time_buyer_sheet.find(username, in_column=1)
+
+    if existing_user:
+        print(f'{chr(10)}Welcome back, {username}')
+    else:
+         validate_username(username)
+         print(f'{chr(10)}Thank you and welcome, {username}')
+
+user_name()
